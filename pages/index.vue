b9727ee8678bd70065fd8a052a9e9cb9ee7c7933
Erfan Mirzapour <erfanmirzapour1@gmail.com>
2025-04-14 13:17:10 +0330

    fine tune

--- pages/index.vue
+++ pages/index.vue
@@ -10,14 +10,14 @@
       </search>
 
       <div v-if="!submitted" class='text-center'>Start searching...</div>
+      <div v-else-if="error" class="text-center" role="alert">{{ error }}</div>
       <div v-else-if="images.length === 0 && !loading || !submitted">There are not images related to your query!
       </div>
       <Loading v-else-if='loading' />
 
       <BContainer v-else fluid class="masonry-container mb-3 text-">
-
          <div v-for="image in images" :key="image.id" class="masonry-item mb-3">
-            <nuxt-img :src="image.images['736x'].url"
+            <NuxtImg loading='lazy' :src="image.images['736x'].url"
                :srcset="`${image.images['236x'].url} 1x, ${image.images['474x'].url} 2x`"
                :width="image.images['736x'].width" :height="image.images['736x'].height" :alt="image.description" />
          </div>
@@ -30,7 +30,7 @@ import type { ImageObject } from '~/types';
 
 const query = ref()
 const images = ref<ImageObject[]>([])
-const error = ref(null)
+const error = ref<null | string>(null)
 const loading = ref(false)
 const submitted = ref(false)
 
@@ -39,16 +39,16 @@ async function onSubmit(e: Event) {
    try {
       loading.value = true
       submitted.value = true
-      const data = await $fetch('/api/images', {
+      const { pins } = await $fetch('/api/images', {
          query: {
             q: query.value,
             limit: 10,
-         }
+         },
       })
-      images.value = data.pins
-      console.log(data.pins)
-   } catch (err) {
-      error.value = err.message
+      images.value = pins
+   } catch (err: any) {
+      console.error(err);
+      error.value = 'Failed to fetch images. Please try again!';
    } finally {
       loading.value = false
    }
--- server/api/images.ts
+++ server/api/images.ts
@@ -3,5 +3,8 @@ export default defineEventHandler(async event => {
    const response = await fetch(
       `https://pingrid-api.fazlali.workers.dev/?q=${query.q}&limit=${query.limit}`
    );
+
+   setResponseStatus(event, response.status);
+
    return response.json();
 });
